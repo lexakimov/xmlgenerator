70d1e18de4b527bcbc1ef947706144067238956a
Alexey Akimov <lex.akimov23@gmail.com>
2025-03-27 12:59:25 +0700

    tests

--- tests/unit/test_configuration.py
+++ tests/unit/test_configuration.py
@@ -6,7 +6,7 @@ import xmlgenerator.configuration as conf
 os.chdir(os.path.dirname(os.path.abspath(tests.__file__)))
 
 
-def test_load_config_none():
+def test_load_config_no_file():
     configuration = conf.load_config(None)
 
     assert configuration
@@ -22,7 +22,7 @@ def test_load_config_none():
     assert len(configuration.specific) == 0
 
 
-def test_load_config_empty():
+def test_load_config_empty_file():
     configuration = conf.load_config('data/config_empty.yaml')
 
     assert configuration
@@ -34,7 +34,7 @@ def test_load_config_empty():
 
 
 def test_load_config_non_empty():
-    configuration = conf.load_config('data/config_non_empty.yaml')
+    configuration = conf.load_config('data/config_value_override.yaml')
 
     assert configuration
     assert configuration.specific is not None
@@ -58,8 +58,8 @@ def test_load_config_non_empty():
     assert configuration.specific['Schema_03'].value_override is not None
 
 
-def test_load_config_non_empty_get_override_1():
-    configuration = conf.load_config('data/config_non_empty.yaml')
+def test_get_for_file_merge_local_and_global_1():
+    configuration = conf.load_config('data/config_value_override.yaml')
     config = configuration.get_for_file("Schema_01")
 
     assert config
@@ -72,8 +72,8 @@ def test_load_config_non_empty_get_override_1():
     assert config.value_override["Отчество"] == "middle_name-2"
 
 
-def test_load_config_non_empty_get_override_2():
-    configuration = conf.load_config('data/config_non_empty.yaml')
+def test_get_for_file_merge_local_and_global_2():
+    configuration = conf.load_config('data/config_value_override.yaml')
     config = configuration.get_for_file("Schema_02")
 
     assert config
@@ -85,8 +85,8 @@ def test_load_config_non_empty_get_override_2():
     assert config.value_override["Имя"] is None
 
 
-def test_load_config_non_empty_get_override_3():
-    configuration = conf.load_config('data/config_non_empty.yaml')
+def test_get_for_file_merge_local_and_global_3():
+    configuration = conf.load_config('data/config_value_override.yaml')
     config = configuration.get_for_file("Schema_03")
 
     assert config
@@ -98,8 +98,8 @@ def test_load_config_non_empty_get_override_3():
     assert config.value_override["Имя"] == "first_name-1"
 
 
-def test_load_config_non_empty_get_override_4():
-    configuration = conf.load_config('data/config_non_empty.yaml')
+def test_get_for_file_merge_local_and_global_4():
+    configuration = conf.load_config('data/config_value_override.yaml')
     config = configuration.get_for_file("Schema_04")
 
     assert config
@@ -109,3 +109,18 @@ def test_load_config_non_empty_get_override_4():
     assert len(config.value_override) == 2
     assert config.value_override["Фамилия"] == "last_name-1"
     assert config.value_override["Имя"] == "first_name-1"
+
+
+def test_load_config_overlapping_patterns():
+    configuration = conf.load_config('data/config_overlapping_patterns.yaml')
+
+    config = configuration.get_for_file("Schema_ABC")
+    assert config.output_filename == "pattern1"
+
+    # Should use the config from the first matching pattern 'Schema_.*'
+    config = configuration.get_for_file("Schema_A")
+    assert config.output_filename == "pattern2"
+
+    # Should use the config from the first matching pattern 'Schema_.*'
+    config = configuration.get_for_file("Schema_B")
+    assert config.output_filename == "pattern2"
