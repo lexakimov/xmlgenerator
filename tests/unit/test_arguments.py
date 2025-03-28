3290a95bd413ca166f00d265cba222d1ab2a4155
Alexey Akimov <lex.akimov23@gmail.com>
2025-03-28 16:55:22 +0700

    refactor tests

--- tests/unit/test_arguments.py
+++ tests/unit/test_arguments.py
@@ -47,7 +47,7 @@ def test_parse_args_version(capsys):
     assert not captured.err
 
 
-def test_parse_args_01(capsys):
+def test_parse_args__one_schema__not_exists(capsys):
     with pytest.raises(SystemExit) as excinfo:
         parse('program ../not_existing.xsd')
 
@@ -57,7 +57,7 @@ def test_parse_args_01(capsys):
     assert 'error: file ../not_existing.xsd doesn\'t exists.' in captured.err
 
 
-def test_parse_args_02(capsys):
+def test_parse_args__one_schema__exists(capsys):
     args, xsd_files, output_path = parse('program data/existing_1.xsd')
 
     assert args.debug is False
@@ -74,24 +74,7 @@ def test_parse_args_02(capsys):
     assert not captured.err
 
 
-def test_parse_args_02_1(capsys):
-    args, xsd_files, output_path = parse('program data/existing_1.xsd data/existing_2.xsd')
-
-    assert args.debug is False
-    assert args.config_yaml is None
-    assert args.encoding == 'utf-8'
-    assert args.output_path is None
-    assert args.pretty is False
-    assert args.seed is None
-    assert len(xsd_files) is 2
-    assert output_path is None
-
-    captured = capsys.readouterr()
-    assert not captured.out
-    assert not captured.err
-
-
-def test_parse_args_02_2(capsys):
+def test_parse_args__one_schema__twice(capsys):
     args, xsd_files, output_path = parse('program data/existing_1.xsd data/existing_1.xsd')
 
     assert args.debug is False
@@ -108,7 +91,48 @@ def test_parse_args_02_2(capsys):
     assert not captured.err
 
 
-def test_parse_args_03(capsys):
+def test_parse_args___two_schemas__exists(capsys):
+    args, xsd_files, output_path = parse('program data/existing_1.xsd data/existing_2.xsd')
+
+    assert args.debug is False
+    assert args.config_yaml is None
+    assert args.encoding == 'utf-8'
+    assert args.output_path is None
+    assert args.pretty is False
+    assert args.seed is None
+    assert len(xsd_files) is 2
+    assert output_path is None
+
+    captured = capsys.readouterr()
+    assert not captured.out
+    assert not captured.err
+
+
+def test_parse_args__one_schema__check_output_is_file(capsys):
+    args, xsd_files, output_path = parse('program -o out.xml data/existing_1.xsd')
+
+    assert output_path is not None
+    assert args.config_yaml is None
+
+
+def test_parse_args__two_schemas__check_output_is_dir(capsys):
+    with pytest.raises(SystemExit) as excinfo:
+        parse('program -o out.xml data/existing_1.xsd data/existing_2.xsd')
+
+    captured = capsys.readouterr()
+    assert excinfo.value.code == 2
+    assert 'usage: xmlgenerator [-h]' in captured.out
+    assert 'error: option -o/--output must be a directory when multiple source xsd schemas are provided.' in captured.err
+
+
+def test_parse_args__two_schemas__create_output_folder(capsys):
+    assert not Path("output_dir").exists()
+    parse('program -o output_dir/ data/existing_1.xsd data/existing_2.xsd')
+    assert Path("output_dir").exists()
+    Path("output_dir").rmdir()
+
+
+def test_parse_args__folder__not_empty(capsys):
     args, xsd_files, output_path = parse('program data/')
 
     assert args.debug is False
@@ -126,7 +150,7 @@ def test_parse_args_03(capsys):
     assert not captured.err
 
 
-def test_parse_args_04(capsys):
+def test_parse_args__folder_empty(capsys):
     with pytest.raises(SystemExit) as excinfo:
         parse('program ./')
 
@@ -136,37 +160,13 @@ def test_parse_args_04(capsys):
     assert 'error: no source xsd schemas provided.' in captured.err
 
 
-def test_parse_args_05(capsys):
-    args, xsd_files, output_path = parse('program -o out.xml data/existing_1.xsd')
-
-    assert output_path is not None
-    assert args.config_yaml is None
-
-
-def test_parse_args_06(capsys):
-    with pytest.raises(SystemExit) as excinfo:
-        parse('program -o out.xml data/existing_1.xsd data/existing_2.xsd')
-
-    captured = capsys.readouterr()
-    assert excinfo.value.code == 2
-    assert 'usage: xmlgenerator [-h]' in captured.out
-    assert 'error: option -o/--output must be a directory when multiple source xsd schemas are provided.' in captured.err
-
-
-def test_parse_args_07(capsys):
-    assert not Path("output_dir").exists()
-    parse('program -o output_dir/ data/existing_1.xsd data/existing_2.xsd')
-    assert Path("output_dir").exists()
-    Path("output_dir").rmdir()
-
-
-def test_parse_args_08(capsys):
+def test_parse_args__config_file_is_empty(capsys):
     args, xsd_files, output_path = parse('program -c data/config_empty.yaml data/existing_1.xsd')
 
     assert args.config_yaml is not None
 
 
-def test_parse_args_09(capsys):
+def test_parse_args__config_file_not_exists(capsys):
     with pytest.raises(SystemExit) as excinfo:
         parse('program -c not_exists.yml data/existing_1.xsd')
 
