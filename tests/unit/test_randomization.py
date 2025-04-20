1009de4846d23cd374f6bf395f66598652c9a9b6
Alexey Akimov <lex.akimov23@gmail.com>
2025-04-20 18:04:45 +0700

    fix test

--- tests/unit/test_randomization.py
+++ tests/unit/test_randomization.py
@@ -13,7 +13,7 @@ def test_random_no_seed():
     randomizer1 = Randomizer()
     randomizer2 = Randomizer()
     for _ in range(10):
-        assert randomizer1.rnd.randint(0, 1000) != randomizer2.rnd.randint(0, 1000)
+        assert randomizer1.integer(0, 1000000) != randomizer2.integer(0, 1000000)
 
 
 @pytest.mark.repeat(10)
@@ -21,7 +21,7 @@ def test_fake_no_seed():
     randomizer1 = Randomizer()
     randomizer2 = Randomizer()
     for _ in range(10):
-        assert randomizer1.fake.name() != randomizer2.fake.name()
+        assert randomizer1.first_name() != randomizer2.first_name()
 
 
 @pytest.mark.repeat(10)
@@ -29,7 +29,7 @@ def test_random_has_seed():
     randomizer1 = Randomizer(seed=123)
     randomizer2 = Randomizer(seed=123)
     for _ in range(10):
-        assert randomizer1.rnd.randint(0, 1000) == randomizer2.rnd.randint(0, 1000)
+        assert randomizer1.integer(0, 1000000) == randomizer2.integer(0, 1000000)
 
 
 @pytest.mark.repeat(10)
@@ -37,4 +37,4 @@ def test_fake_has_seed():
     randomizer1 = Randomizer(seed=123)
     randomizer2 = Randomizer(seed=123)
     for _ in range(10):
-        assert randomizer1.fake.name() == randomizer2.fake.name()
+        assert randomizer1.first_name() == randomizer2.first_name()
