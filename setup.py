3ffe493774c6e2c1071d28757fd85edb5b779207
Alexey Akimov <lex.akimov23@gmail.com>
2025-07-01 12:09:36 +0700

    Add Python setup and dependency installation to release workflow

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/lexakimov/xmlgenerator',
+    license='MIT',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
